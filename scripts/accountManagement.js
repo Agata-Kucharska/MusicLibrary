96e8590f0ca76c0801474b28b1d9d25249131d38
MichalGomulka <48628200+MichalGomulka@users.noreply.github.com>
2020-05-17 11:17:00 +0200

    Enhance #24

--- scripts/accountManagement.js
+++ scripts/accountManagement.js
@@ -85,4 +85,5 @@ const logout = document.querySelector('#logout');
 logout.addEventListener('click', (e) => {
   e.preventDefault();
   auth.signOut();
+  window.location.replace("index.html");
 });
